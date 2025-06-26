727440653ed232526429fa121297b4e35e9fdc23
Vishal Gurung <goblin01@g0>
2025-06-26 23:46:44 +0900

    updating index.html

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/g0/',  // Replace 'g0' with your exact GitHub repo name
   plugins: [react()],
 })
