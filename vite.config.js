cffb5799446d215882e07a663238e506084b28b0
g0 <grgvishal.gurung17@gmail.com>
2025-08-19 23:47:07 +0900

    remove domain

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/',  // Replace 'g0' with your exact GitHub repo name
+  base: process.env.NODE_ENV === 'production' ? '/g0/' : '/',
+ // Replace 'g0' with your exact GitHub repo name
   plugins: [react()],
-})
+});
