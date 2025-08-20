f1cf119d9e86ef33b399663e2063fa6eb03c08b8
g0 <grgvishal.gurung17@gmail.com>
2025-08-20 18:47:12 +0900

    router fix

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/g0/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/g0' : '/',
  // Replace 'g0' with your exact GitHub repo name
   plugins: [react()],
 });
